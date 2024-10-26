effef6c940d482fe60b2ea41cd344ff3df5388a3
Ritik Ranjan <ritikran@stones.cse.buffalo.edu>
2024-10-26 18:45:40 -0400

    dbgg

--- Grader/remote/grader_remote.py
+++ Grader/remote/grader_remote.py
@@ -154,8 +154,8 @@ class GetHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         try:
-            response = test_runner(action, binary, args)
-            self.wfile.write(response.encode('utf-8'))  # Ensure response is in bytes
+            # response = test_runner(action, binary, args)
+            self.wfile.write(test_runner(action, binary, args))  # Ensure response is in bytes
         except Exception as e:
             # Handle exceptions and log error
             print(f"Error processing request: {e}")
