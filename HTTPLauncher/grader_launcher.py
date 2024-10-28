548559959072f840e01df6e354eb61a261298e74
Ritik Ranjan <ritikran@stones.cse.buffalo.edu>
2024-10-28 04:48:14 -0400

    summary debug

--- HTTPLauncher/grader_launcher.py
+++ HTTPLauncher/grader_launcher.py
@@ -135,8 +135,9 @@ class HTTPHandler(BaseHTTPRequestHandler):
         
         # Send both response and summary
         try:
-            summary_message = "Summary:" + " ".join(summary)
-            self.wfile.write((response + " " + summary_message).encode('utf-8'))
+            # summary_message = "Summary:" + " ".join(summary)
+            # self.wfile.write((response + " " + summary_message).encode('utf-8'))
+            self.wfile.write(response)
         except ValueError as e:
             print(f"Error writing response: {str(e)}")
 
@@ -154,7 +155,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         # Correct way to send summary
-        self.wfile.write((b'OK\n' + summary_message.encode('utf-8')))
+        self.wfile.write(b'OK\n') # + summary_message.encode('utf-8'))
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
